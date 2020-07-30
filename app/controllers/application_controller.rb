a640471a8df24d13828704a18f077077ad1176eb
Joacir Oliveira Santos Júnior <jr.joacir@gmail.com>
2020-07-30 19:50:29 -0300

    Create Client create action and rescue errors record_not_found and record_invalid in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+  # how to get exceptions message and put them in json?
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+
+  private
+
+  def record_not_found
+    render json: { message: 'Not Found' }, status: 404
+  end
+
+  def record_invalid
+    render json: { message: 'Unprocessable Entity' }, status: 422
+  end
 end
--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -6,4 +6,14 @@ class ClientsController < ApplicationController
   def show
     render json: Client.find(params[:id])
   end
+
+  def create
+    render json: Client.create!(create_params), status: 201
+  end
+
+  private
+
+  def create_params
+    params.require(:client).permit(:name)
+  end
 end
