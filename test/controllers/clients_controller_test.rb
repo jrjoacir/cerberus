229a6c96fbc5c845a9cdca9302bf4f4672ae0e03
Joacir Oliveira Santos Júnior <jr.joacir@gmail.com>
2020-08-02 19:58:04 -0300

    Implemented test cases for products controller

--- test/controllers/clients_controller_test.rb
+++ test/controllers/clients_controller_test.rb
@@ -1,28 +1,28 @@
 require 'test_helper'
 
 class ClientsControllerTest < ActionDispatch::IntegrationTest
-  test 'should be return all clients' do
+  test 'should return all clients' do
     get '/clients'
     assert_response :success
     assert_equal @response.body, clients.to_json
     assert_equal @response.code, '200'
   end
 
-  test 'should be return all clients filter by product' do
+  test 'should return all clients filter by product' do
     get "/products/#{products(:one).id}/clients"
     assert_response :success
     assert_equal @response.body, [clients_products(:one).client, clients_products(:two).client].to_json
     assert_equal @response.code, '200'
   end
 
-  test 'should be return one client' do
+  test 'should return one client' do
     get "/clients/#{clients(:one).id}"
     assert_response :success
     assert_equal @response.body, clients(:one).to_json
     assert_equal @response.code, '200'
   end
 
-  test 'should be raise not found' do
+  test 'should raise not found' do
     get "/clients/-#{clients(:one).id}"
     assert_response :missing
     assert_equal @response.body, { message: 'Not Found' }.to_json
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,47 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should return all products' do
+    get '/products'
+    assert_response :success
+    assert_equal @response.body, products.to_json
+    assert_equal @response.code, '200'
+  end
+
+  test 'should return all products filter by client' do
+    get "/clients/#{clients(:one).id}/products"
+    assert_response :success
+    assert_equal @response.body, [clients_products(:one).product].to_json
+    assert_equal @response.code, '200'
+  end
+
+  test 'should return one product' do
+    get "/products/#{products(:one).id}"
+    assert_response :success
+    assert_equal @response.body, products(:one).to_json
+    assert_equal @response.code, '200'
+  end
+
+  test 'should raise not found' do
+    get "/products/-#{products(:one).id}"
+    assert_response :missing
+    assert_equal @response.body, { message: 'Not Found' }.to_json
+    assert_equal @response.code, '404'
+  end
+
+  test 'should create a product' do
+    assert_difference('Product.count', 1) do
+      post '/products', params: { product: { name: 'Product-test', description: 'Product test description' } }
+      assert_response :success
+      assert_equal @response.code, '201'
+    end
+  end
+
+  test 'should raise a error on create a product' do
+    assert_no_difference('Product.count') do
+      post '/products', params: { product: { name: products(:one).name } }
+      assert_equal @response.body, { message: 'Unprocessable Entity' }.to_json
+      assert_equal @response.code, '422'
+    end
+  end
 end
