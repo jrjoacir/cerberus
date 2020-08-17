0850e09a39fbc62e102e95a46783609ae1f32729
Joacir Oliveira Santos Júnior <jr.joacir@gmail.com>
2020-08-17 20:41:42 -0300

    Create endpoint to create a product-client association

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :clients, only: [:create, :index, :show] do
-    resources :products, only: [:index, :show]
+    resources :products, only: [:index, :show] do
+      post '', to: 'clients_product#create'
+    end
   end
 
   resources :products, only: [:create, :index, :show] do
-    resources :clients, only: [:index, :show]
     resources :features, only: [:create, :index, :show]
+    resources :clients, only: [:index, :show] do
+      post '', to: 'clients_product#create'
+    end
   end
 
   resources :users, only: [:index, :show, :create]
