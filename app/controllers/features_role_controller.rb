0271b4906d3b6deebff5be24e7ad953a7d541b56
Joacir Oliveira Santos Júnior <jr.joacir@gmail.com>
2020-10-25 15:30:40 -0300

    Refactorin FeaturesRoleController

--- app/controllers/features_role_controller.rb
+++ app/controllers/features_role_controller.rb
@@ -1,21 +1,16 @@
 class FeaturesRoleController < ApplicationController
   def create
-    render json: FeaturesRole.create!(create_params), status: 201
+    render json: FeaturesRole.create!(valid_params), status: 201
   end
 
   def destroy
-    FeaturesRole.destroy_by(delete_params)
+    FeaturesRole.destroy_by(valid_params)
     render status: 204
   end
 
   private
 
-  def create_params
-    params.permit(:feature_id, :role_id)
-    { features_id: params[:feature_id], roles_id: params[:role_id] }
-  end
-
-  def delete_params
+  def valid_params
     params.permit(:feature_id, :role_id)
     { features_id: params[:feature_id], roles_id: params[:role_id] }
   end
