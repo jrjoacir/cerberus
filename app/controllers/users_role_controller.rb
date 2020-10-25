6dc66014c6a8c754870fa484628db46a8dbd96ef
Joacir Oliveira Santos Júnior <jr.joacir@gmail.com>
2020-10-25 15:20:22 -0300

    Refactorin UsersRoleController

--- app/controllers/users_role_controller.rb
+++ app/controllers/users_role_controller.rb
@@ -1,21 +1,16 @@
 class UsersRoleController < ApplicationController
   def create
-    render json: UsersRole.create!(create_params), status: 201
+    render json: UsersRole.create!(valid_params), status: 201
   end
 
   def destroy
-    UsersRole.destroy_by(delete_params)
+    UsersRole.destroy_by(valid_params)
     render status: 204
   end
 
   private
 
-  def create_params
-    params.permit(:user_id, :role_id)
-    { users_id: params[:user_id], roles_id: params[:role_id] }
-  end
-
-  def delete_params
+  def valid_params
     params.permit(:user_id, :role_id)
     { users_id: params[:user_id], roles_id: params[:role_id] }
   end
