e85cdfe03a5e7e724e2287c50527bce3397b890d
Joacir Oliveira Santos Júnior <jr.joacir@gmail.com>
2020-11-27 23:06:09 -0300

    Rascunho de criação de healthcheck

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
 
+  def healthcheck
+    database_status = check_database_connection!
+    status = 500 if [:error, :fail].include?(database_status[:status])
+    render json: { database: database_status }, status: status || 200
+  end
+
   private
 
   def record_not_found
@@ -11,5 +17,15 @@ class ApplicationController < ActionController::API
 
   def record_invalid
     render json: { message: 'Unprocessable Entity' }, status: 422
+  end
+
+  def check_database_connection!
+    start = Time.now
+    ActiveRecord::Base.establish_connection
+    ActiveRecord::Base.connection
+    return { message: 'Database Connection Success', duration: (Time.now - start).to_f, status: :success } if ActiveRecord::Base.connected?
+    { message: 'Database Connection Failed', duration: (Time.now - start).to_f, status: :fail }
+  rescue
+    { message: 'Database Connection Failed', duration: (Time.now - start).to_f, status: :error }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/healthcheck', to: 'application#healthcheck'
   resources :clients, only: %i[create index show update] do
     resources :products, only: %i[index show] do
       resources :roles, only: %i[index show destroy]
